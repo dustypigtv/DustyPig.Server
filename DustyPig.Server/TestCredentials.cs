5aa73242414de4eb02d6d962a396617f9c6b90b1
JD <dev@dustypig.tv>
2022-04-07 23:58:02 -0400

    Remove Test-User password, now that it's been changed in Firebase to be a secret

--- DustyPig.Server/TestCredentials.cs
+++ DustyPig.Server/TestCredentials.cs
@@ -6,7 +6,6 @@
         public const int ProfileId = 1;
         public const string FirebaseId = "qVp7jUVVTgS0HAQLd33HCsTmUeI2";
         public const string Email = "testuser@dustypig.tv";
-        public const string Password = "test^Pass1";
         public const string Name = "Test User";
     }
 }
