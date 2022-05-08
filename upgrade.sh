fd8f5705817eae2c69d75548d168735f8c6d8734
JD <dev@dustypig.tv>
2022-05-08 14:14:29 -0600

    Add upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+mkdir -p _work_
+
+wget -O _work_/DustyPig.Server.zip https://github.com/dustypigtv/DustyPig.Server/releases/latest/download/DustyPig.Server.zip
+
+unzip _work_/DustyPig.Server.zip -d _work_/server
+
+sudo systemctl stop service-dustypig-tv
+sudo mkdir -p /var/www/service.dustypig.tv
+sudo rclone sync _work_/server /var/www/service.dustypig.tv -v --checksum
+sudo systemctl start service-dustypig-tv
+
+rm -rf _work_
