ff358c00d94578b1c80673aaae67efe30f46ff70
JD <dev@dustypig.tv>
2022-07-01 13:56:22 -0600

    Simplify GetRequest migration

--- DustyPig.Server/Data/Migrations/20220701195528_SimplifyGetRequests.Designer.cs
+++ DustyPig.Server/Data/Migrations/20220701195528_SimplifyGetRequests.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DustyPig.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DustyPig.Server.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220701195528_SimplifyGetRequests")]
+    partial class SimplifyGetRequests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
