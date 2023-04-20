199358720ae144f019f4063ac171b602c1a7e647
JD <dev@dustypig.tv>
2023-04-20 00:04:34 -0600

    Add a unique index

--- DustyPig.Server/Data/Models/FCMToken.cs
+++ DustyPig.Server/Data/Models/FCMToken.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace DustyPig.Server.Data.Models
 {
+    [Index(nameof(ProfileId), nameof(TokenHash), IsUnique = true)]
     public class FCMToken
     {
         public int Id { get; set; }
@@ -13,6 +17,10 @@ namespace DustyPig.Server.Data.Models
         [Required]
         [MaxLength(API.v3.Models.Constants.MAX_MOBILE_DEVICE_ID_LENGTH)]
         public string Token { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string TokenHash { get; set; }
 
         public DateTime LastSeen { get; set; }
     }
