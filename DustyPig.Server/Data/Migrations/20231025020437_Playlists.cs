ab4e82833a7c11d96b5d08fb835db8346a0680cc
JD <dev@dustypig.tv>
2023-10-24 20:05:30 -0600

    Playlist migration

---
+++ DustyPig.Server/Data/Migrations/20231025020437_Playlists.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DustyPig.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Playlists : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Playlists_ProfileId_Name_CurrentIndex",
+                table: "Playlists");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrentIndex",
+                table: "Playlists",
+                newName: "CurrentItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Playlists_ProfileId_Name",
+                table: "Playlists",
+                columns: new[] { "ProfileId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Playlists_ProfileId_Name",
+                table: "Playlists");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrentItemId",
+                table: "Playlists",
+                newName: "CurrentIndex");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Playlists_ProfileId_Name_CurrentIndex",
+                table: "Playlists",
+                columns: new[] { "ProfileId", "Name", "CurrentIndex" },
+                unique: true);
+        }
+    }
+}
