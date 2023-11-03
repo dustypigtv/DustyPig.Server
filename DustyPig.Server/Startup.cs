62cf397b291387b75440ebc574ef6b201642cd67
JD <dev@dustypig.tv>
2023-11-02 18:18:47 -0600

    Simplify CORS

--- DustyPig.Server/Startup.cs
+++ DustyPig.Server/Startup.cs
@@ -100,6 +100,17 @@ namespace DustyPig.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
             services.AddMemoryCache();
 
             services.AddDbContext<AppDbContext>();
