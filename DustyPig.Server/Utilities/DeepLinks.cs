bc485d457e08b656e7ec100028ced1144c6f85ec
Jason <dev@dustypig.tv>
2024-01-15 15:18:15 -0700

    Add deep link to request path in notifications

--- DustyPig.Server/Utilities/DeepLinks.cs
+++ DustyPig.Server/Utilities/DeepLinks.cs
@@ -7,20 +7,35 @@ namespace DustyPig.Server.Utilities
     {
         public static string Create(Notification notification)
         {
+
             if (notification.NotificationType == NotificationType.Friend)
                 return $"friendship/{notification.FriendshipId}";
 
-            if (notification.NotificationType == NotificationType.GetRequest)
-                return $"requests/{notification.GetRequestId}";
-
-
 
             var mediaTypes = new NotificationType[]
-            {
+{
                 NotificationType.Media,
                 NotificationType.OverrideRequest
             };
 
+            if (notification.NotificationType == NotificationType.GetRequest)
+            {
+                if (mediaTypes.Contains(notification.NotificationType) && notification.MediaEntry != null)
+                {
+                    switch (notification.MediaEntry.EntryType)
+                    {
+                        case API.v3.Models.MediaTypes.Movie:
+                            return $"requests/movie/{notification.MediaEntryId}";
+                        case API.v3.Models.MediaTypes.Series:
+                            return $"requests/series/{notification.MediaEntryId}";
+                        default:
+                            return null;
+                    }
+                }
+            }
+
+
+            
             if (mediaTypes.Contains(notification.NotificationType) && notification.MediaEntry != null)
             {
                 switch (notification.MediaEntry.EntryType)
