f77431c35ed04e5188acc5d5b724c26c9651cf80
Jason Davis <jasondavis303@gmail.com>
2025-08-02 14:33:39 -0600

    Fix consumes bug

--- DustyPig.Server/Controllers/v3/_BaseController.cs
+++ DustyPig.Server/Controllers/v3/_BaseController.cs
@@ -8,7 +8,7 @@ namespace DustyPig.Server.Controllers.v3
     [ApiVersion("3")]
     [Route("api/v{version:apiVersion}/[controller]/[action]")]
     [Produces("application/json")]
-    [Consumes("application/json")]
+    [Consumes("application/json", IsOptional = true)]
     public abstract class _BaseController : Controller
     {
         public _BaseController(AppDbContext db) => DB = db;
