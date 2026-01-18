cfa61f984d8d1e595ebcf93e5fb8407a05fb8716
Jason Davis <jasondavis303@gmail.com>
2026-01-17 23:37:17 -0700

    Remote Krypto dependency

--- DustyPig.Server/Utilities/Crypto.cs
+++ DustyPig.Server/Utilities/Crypto.cs
@@ -1,24 +1,30 @@
-﻿using Krypto.WonderDog.Hashers;
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 
-namespace DustyPig.Server.Utilities
+namespace DustyPig.Server.Utilities;
+
+
+public static partial class Crypto
 {
-    public static class Crypto
+    private static readonly SHA512 _hasher = SHA512.Create();
+
+    [GeneratedRegex("[^\\w]")]
+    private static partial Regex NormalizedHashRegex();
+
+
+    public static string HashString(string value) => Convert.ToHexString(_hasher.ComputeHash(Encoding.UTF8.GetBytes(value + string.Empty)));
+
+    public static string NormalizedHash(string title)
     {
-        private static readonly IHasher _hasher = HasherFactory.CreateSHA512();
-
-        public static string HashString(string value) => _hasher.Hash(value).Replace("-", null);
-
-        public static string NormalizedHash(string title)
-        {
-            title = (title + string.Empty).Trim();
-            title = title.ToLower();
-            title = Regex.Replace(title, "[^\\w]", string.Empty);
-            return HashString(title);
-        }
-
-        public static string HashMovieTitle(string title, int year) => NormalizedHash($"{title}{year}");
-
-        public static string HashEpisode(int seriesId, int season, int episode) => HashString($"{seriesId}.{season}.{episode}");
+        title = (title + string.Empty).Trim();
+        title = title.ToLower();
+        title = NormalizedHashRegex().Replace(title, string.Empty);
+        return HashString(title);
     }
+
+    public static string HashMovieTitle(string title, int year) => NormalizedHash($"{title}{year}");
+
+    public static string HashEpisode(int seriesId, int season, int episode) => HashString($"{seriesId}.{season}.{episode}");
 }
