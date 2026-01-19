645718aa6a3ce86f8485a69396dcf2b5cb6c56de
Jason Davis <jasondavis303@gmail.com>
2026-01-18 18:47:17 -0700

    Cleanup AppDbContext

--- DustyPig.Server/Data/AppDbContext.cs
+++ DustyPig.Server/Data/AppDbContext.cs
@@ -3,10 +3,12 @@ using DustyPig.API.v3.MPAA;
 using DustyPig.Server.Controllers.v3.Logic;
 using DustyPig.Server.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.DependencyInjection;
 using NuGet.Protocol.Plugins;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -94,6 +96,81 @@ namespace DustyPig.Server.Data
 
 
 
+        public string GetTableName<TEntity>() where TEntity : class
+        {
+            IEntityType entityType = Model.FindEntityType(typeof(TEntity));
+            return entityType.GetTableName()!;
+        }
+
+        public async Task<DbConnection> GetOpenDbConnection(CancellationToken cancellationToken)
+        {
+            var conn = Database.GetDbConnection();
+            if (conn.State != System.Data.ConnectionState.Open)
+                await conn.OpenAsync(cancellationToken);
+            return conn;
+        }
+
+
+
+        /// <summary>
+        /// This calls <see cref="DbContext.SaveChangesAsync(bool, CancellationToken)"/> on <paramref name="db"/>
+        /// </summary>
+        public static async Task MarkPlaylistArtworkNeedsupdate(this AppDbContext db, List<int> ids, CancellationToken cancellationToken = default)
+        {
+            if (ids == null || ids.Count == 0)
+                return;
+
+            var playlists = await db.Playlists
+                .Where(_ => ids.Contains(_.Id))
+                .ToListAsync(cancellationToken);
+
+            if (playlists.Count == 0)
+                return;
+
+            playlists.ForEach(_ => _.ArtworkUpdateNeeded = true);
+            await db.SaveChangesAsync(cancellationToken);
+        }
+
+
+
+
+
+
+        public static async Task<List<int>> GetLibraryIdsAccessableByAccount(this AppDbContext db, int accountId)
+        {
+            //Libs owned by the account
+            var libs = await db.Libraries
+                .AsNoTracking()
+                .Where(item => item.AccountId == accountId)
+                .Select(item => item.Id)
+                .ToListAsync();
+
+
+            //Libs shared with the account
+            var sharedLibs = await db.FriendLibraryShares
+                .AsNoTracking()
+
+                .Include(item => item.Friendship)
+                .ThenInclude(item => item.Account1)
+                .ThenInclude(item => item.Profiles)
+
+                .Include(item => item.Friendship)
+                .ThenInclude(item => item.Account2)
+                .ThenInclude(item => item.Profiles)
+
+                .Include(item => item.Library)
+
+                .Where(item => item.Friendship.Account1Id == accountId || item.Friendship.Account2Id == accountId)
+                .Where(item => item.Friendship.Accepted)
+                .Where(item => !libs.Contains(item.LibraryId))
+
+                .Select(item => item.LibraryId)
+                .ToListAsync();
+
+            libs.AddRange(sharedLibs);
+
+            return libs;
+        }
 
 
 
