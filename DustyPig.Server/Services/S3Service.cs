12e7c8f57e4f7c254c59f3544c5d0f7c732286d5
Jason Davis <jasondavis303@gmail.com>
2026-02-13 08:45:17 -0700

    Fix bug where listing no longer works with delimeter

--- DustyPig.Server/Services/S3Service.cs
+++ DustyPig.Server/Services/S3Service.cs
@@ -69,7 +69,12 @@ public class S3Service : IDisposable
         {
             var ret = new List<S3Object>();
 
-            var request = new ListObjectsV2Request { BucketName = Constants.DEFAULT_HOST, Prefix = prefix, Delimiter = "/" };
+            var request = new ListObjectsV2Request 
+            {
+                BucketName = Constants.DEFAULT_HOST,
+                Prefix = prefix
+            };
+            
             ListObjectsV2Response response;
             do
             {
@@ -120,27 +125,15 @@ public class S3Service : IDisposable
         {
             if (disposing)
             {
-                // TODO: dispose managed state (managed objects)
                 _client.Dispose();
                 _transferUtility.Dispose();
             }
-
-            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-            // TODO: set large fields to null
             _disposed = true;
         }
     }
 
-    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-    // ~S3Service()
-    // {
-    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-    //     Dispose(disposing: false);
-    // }
-
     public void Dispose()
     {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
         Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
