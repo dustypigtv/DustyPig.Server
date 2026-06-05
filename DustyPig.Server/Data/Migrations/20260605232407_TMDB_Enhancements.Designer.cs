d713e9a972a1dad4c2724aea7233df5b06044bd7
Jason Davis <jasondavis303@gmail.com>
2026-06-05 17:24:44 -0600

    TMDB enhancement migration

--- DustyPig.Server/Data/Migrations/20260605232407_TMDB_Enhancements.Designer.cs
+++ DustyPig.Server/Data/Migrations/20260605232407_TMDB_Enhancements.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DustyPig.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DustyPig.Server.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260605232407_TMDB_Enhancements")]
+    partial class TMDB_Enhancements
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DustyPig.Server/Data/Migrations/20260605232407_TMDB_Enhancements.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DustyPig.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TMDB_Enhancements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailureCount",
+                table: "TMDB_Entries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Genres",
+                table: "TMDB_Entries",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailureCount",
+                table: "TMDB_Entries");
+
+            migrationBuilder.DropColumn(
+                name: "Genres",
+                table: "TMDB_Entries");
+        }
+    }
+}
